e4a64e7d2aec480d936f58407435ac94f5e9ca9b
tobbensol <tobsol01@gmail.com>
2023-10-13 18:37:24 +0200

    initial work on message history gathering

--- commands/test_commands.py
+++ commands/test_commands.py
@@ -28,11 +28,21 @@ class EditWindow(discord.ui.Modal):
 )
 async def modal_test(ctx: discord.Interaction):
     edit = EditWindow("test", 1, 2)
-    # TODO find a way to prevent the assignment to happen immediately, and rather wait until the user has finished writing
     await ctx.response.send_modal(edit)
 
+    # TODO this works, but it sucks
     while(not edit.submitted):
         await asyncio.sleep(1)
     
     print(type(edit.children[0]))
     print(type(literal_eval(str(edit.children[1]))))
+
+@tree.command(
+    name="collect_data",
+    description="testing data collection",
+    guild=discord.Object(id=guild_id)
+)
+@app_commands.checks.bot_has_permissions(read_message_history = True)
+async def collect_data(ctx: discord.Interaction):
+    data = [[message.author.id, message.content] async for message in ctx.channel.history(limit = 10, )]
+    print(data)
