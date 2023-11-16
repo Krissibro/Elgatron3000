f792e07a55a9b3acef7ed54f7299f98fbab0fe57
Kristoffer <kje022@uib.no>
2023-11-16 19:12:15 +0100

    Refactored some code in epic_games_commands.py for cleaner UI

--- commands/epic_games_commands.py
+++ commands/epic_games_commands.py
@@ -3,17 +3,17 @@ from epicstore_api import EpicGamesStoreAPI
 from utilities.shared import *
 
 
-async def post_free_games(channel):
+async def make_link_embed():
+    embed = discord.Embed(title="Free Games INCOMING!!!!")
+    embed.description = (f"[Epic Games](https://store.epicgames.com/en-US/free-games)\n" +
+                         f"[Playstation Games](https://www.playstation.com/en-us/ps-plus/whats-new/#monthly-games)")
+
+    return embed
+
+
+async def post_free_epic_games(channel):
     api = EpicGamesStoreAPI()
     free_games = api.get_free_games()["data"]["Catalog"]["searchStore"]["elements"]
-
-    epic_games_embed = discord.Embed(title="Free Epic Games!!!",
-                                     description="https://store.epicgames.com/en-US/free-games")
-    await channel.send(embed=epic_games_embed)
-
-    playstation_embed = discord.Embed(title="Free Piss Games!!!",
-                                      description="https://www.playstation.com/en-us/ps-plus/whats-new/#monthly-games")
-    await channel.send(embed=playstation_embed)
 
     for game in free_games:
         # Check if there is a promotion
@@ -28,7 +28,7 @@ async def post_free_games(channel):
                     embed = discord.Embed(title=game["title"], description=game["description"])
 
                     for image in game["keyImages"]:
-                        if image["type"] == "Thumbnail":
+                        if image["type"] == "OfferImageWide":
                             embed.set_image(url=image["url"])
 
                     await channel.send(embed=embed)
@@ -40,19 +40,17 @@ async def post_free_games(channel):
     guild=discord.Object(id=guild_id)
 )
 async def free_games_rn(ctx):
-    await ctx.response.send_message(embed=discord.Embed(title="Free Games INCOMING!!!!"))
-    await post_free_games(ctx.channel)
+    await ctx.response.send_message(embed=await make_link_embed())
+    await post_free_epic_games(ctx.channel)
 
 
 async def scheduled_post_free_games():
     channel = client.get_channel(1111353625638350893)
-
-    await channel.send(embed=discord.Embed(title="Free Games INCOMING!!!!"))
-    await post_free_games(channel)
+    await channel.send(embed=await make_link_embed())
+    await post_free_epic_games(channel)
 
 
 async def schedule_post_free_games():
-
     trigger = CronTrigger(day_of_week='thu', hour=18, minute=0, second=0, timezone='Europe/Oslo')
     scheduler.add_job(scheduled_post_free_games, trigger)
     scheduler.start()
