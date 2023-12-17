8f903a7cd379fbd3a87446a8f4f81e763a93ac67
Tobias Soltvedt <tobsol01@gmail.com>
2023-12-17 02:50:06 +0100

    added emoji index to command management menu

--- command_objects/CommandInfo.py
+++ command_objects/CommandInfo.py
@@ -28,3 +28,21 @@ class CommandInfo(ABC):
 
     # async def delete_messages_except_first(self):
     #     await self.channel.delete_messages(self.messages[1:])
+
+    # TODO: find a good place for this, i think it could be used elsewhere, so who knows where we should put it
+    @classmethod
+    def id_to_emoji(cls, command_id: int) -> str:
+        emoji_dict = {"0": ":zero:",
+                      "1": ":one:",
+                      "2": ":two:",
+                      "3": ":three:",
+                      "4": ":four:",
+                      "5": ":five:",
+                      "6": ":six:",
+                      "7": ":seven:",
+                      "8": ":eight:",
+                      "9": ":nine:"}
+        temp = ""
+        for i in str(command_id):
+            temp += emoji_dict[i]
+        return temp
--- command_objects/MessagingInfo.py
+++ command_objects/MessagingInfo.py
@@ -18,20 +18,20 @@ class MessagingInfo(CommandInfo):
             title=f"Command: {self.command}",
             description=f"Message: {self.message}"
         )
-        embed.add_field(name="User:", value=f"@{self.user}", inline=False)
+        embed.add_field(name="User:", value=f"{self.user}", inline=False)
         embed.add_field(name="Amount:", value=f"{self.remaining}/{self.amount}", inline=True)
         embed.add_field(name="Interval:", value=f"{timedelta(seconds=self.interval)}", inline=True)
         return embed
 
     def make_overview(self, index, embed):
         formatted_command_string = " ".join(self.command.split("_")).capitalize()
-        embed.add_field(name=f"{index}. {formatted_command_string}",
+        embed.add_field(name=f"{CommandInfo.id_to_emoji(index)} {formatted_command_string}",
                         value=f"{self.user}\n{self.message}",
                         inline=False)
 
         return embed
 
     def make_select_option(self, index):
-        return discord.SelectOption(label=f"{index}.",
+        return discord.SelectOption(label=f"{index}:",
                                     value=str(index),
                                     description=f"{self.message}")
