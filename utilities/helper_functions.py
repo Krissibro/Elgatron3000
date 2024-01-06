960d3228cf1b8d0ea3624666df301be0a24b81ab
Tobias Soltvedt <tobsol01@gmail.com>
2024-01-07 00:37:34 +0100

    added a char to emoji function

--- utilities/helper_functions.py
+++ utilities/helper_functions.py
@@ -12,7 +12,6 @@ def parse_time(time_str: str) -> int:
 
 
 def format_seconds(seconds: int) -> str:
-
     formatted_time = ""
     for unit, divisor in [('d', 86400), ('h', 3600), ('m', 60), ('s', 1)]:
         value, seconds = divmod(seconds, divisor)
@@ -20,6 +19,51 @@ def format_seconds(seconds: int) -> str:
             formatted_time += f"{value}{unit}"
 
     return formatted_time if formatted_time else "0s"
+
+
+def char_to_emoji(command_id) -> str:
+    emoji_dict = {"0": "0️⃣",
+                  "1": "1️⃣",
+                  "2": "2️⃣",
+                  "3": "3️⃣",
+                  "4": "4️⃣",
+                  "5": "5️⃣",
+                  "6": "6️⃣",
+                  "7": "7️⃣",
+                  "8": "8️⃣",
+                  "9": "9️⃣",
+                  "a": "🇦",
+                  "b": "🇧",
+                  "c": "🇨",
+                  "d": "🇩",
+                  "e": "🇪",
+                  "f": "🇫",
+                  "g": "🇬",
+                  "h": "🇭",
+                  "i": "🇮",
+                  "j": "🇯",
+                  "k": "🇰",
+                  "l": "🇱",
+                  "m": "🇲",
+                  "n": "🇳",
+                  "o": "🇴",
+                  "p": "🇵",
+                  "q": "🇶",
+                  "r": "🇷",
+                  "s": "🇸",
+                  "t": "🇹",
+                  "u": "🇺",
+                  "v": "🇻",
+                  "w": "🇼",
+                  "x": "🇽",
+                  "y": "🇾",
+                  "z": "🇿"
+                  }
+    temp = ""
+    print(command_id)
+    for i in str(command_id).lower():
+        temp += emoji_dict[str(i)]
+    return temp
 
 
 if __name__ == "__main__":
