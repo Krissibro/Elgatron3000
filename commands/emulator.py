6f465c34ce3074abd586a7b8208d9fb276214769
tobbensol <tobsol01@gmail.com>
2024-03-21 20:25:11 +0100

    increased frame per button to 200

--- commands/emulator.py
+++ commands/emulator.py
@@ -41,54 +41,52 @@ async def pass_time(ctx, time: int):
 class EmulatorController(discord.ui.View):
 
     def __init__(self, message_ctx):
-        super().__init__()
+        super().__init__(timeout=None)
         self.message_ctx = message_ctx
 
     async def update_gif(self):
         file = discord.File(fp=emu.make_gif(), filename="emulator.gif")
 
-        await self.message_ctx.edit_original_response(attachments=[file], view=EmulatorController(self.message_ctx))
-
-        self.stop()
+        await self.message_ctx.edit_original_response(attachments=[file], view=self)
 
     @discord.ui.button(emoji="🅰️", style=discord.ButtonStyle.blurple, row=0)
     async def A_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
-        emu.sim_button_time("A", 120)
+        emu.sim_button_time("A", 200)
 
         await self.update_gif()
 
     @discord.ui.button(emoji="🔼", style=discord.ButtonStyle.blurple, row=0)
     async def up_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
-        emu.sim_button_time("up", 120)
+        emu.sim_button_time("up", 200)
 
         await self.update_gif()
 
     @discord.ui.button(emoji="🅱️", style=discord.ButtonStyle.blurple, row=0)
     async def B_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
-        emu.sim_button_time("B", 120)
+        emu.sim_button_time("B", 200)
 
         await self.update_gif()
 
     @discord.ui.button(emoji="◀️", style=discord.ButtonStyle.blurple, row=1)
     async def left_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
-        emu.sim_button_time("Left", 120)
+        emu.sim_button_time("Left", 200)
 
         await self.update_gif()
 
     @discord.ui.button(emoji="🔽", style=discord.ButtonStyle.blurple, row=1)
     async def down_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
-        emu.sim_button_time("Down", 120)
+        emu.sim_button_time("Down", 200)
 
         await self.update_gif()
 
     @discord.ui.button(emoji="▶️", style=discord.ButtonStyle.blurple, row=1)
     async def right_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
-        emu.sim_button_time("Right", 120)
+        emu.sim_button_time("Right", 200)
 
         await self.update_gif()
