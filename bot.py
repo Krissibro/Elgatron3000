8bbf4584ac1bd28935b4b6c885dee80d9cd20514
tobbensol <tobsol01@gmail.com>
2024-03-27 20:09:42 +0100

    fixed merge issue

--- bot.py
+++ bot.py
@@ -3,9 +3,14 @@ import discord
 from dotenv import load_dotenv
 
 # Commands from other files
-from commands.epic_games_commands import schedule_post_free_games
-from commands.wordle import initialize_wordle
-from commands.guess_that_pin import initialize_guess_that_pin
+from commands.command_management import manage_commands, kill_all_commands, cleanup
+from commands.messaging_commands import annoy, get_attention, dm_spam
+from commands.polls import start_poll
+from commands.epic_games_commands import schedule_post_free_games, free_games_rn
+from commands.wordle import initialize_wordle, wordle, guess_wordle
+from commands.random_commands import this_dude, weave, pet_elga3, thanos_snapped, trout
+from commands.help import help_command
+from commands.guess_that_pin import guess_that_pin, initialize_guess_that_pin
 from commands.emulator_commands import pokemon, emu
 
 from utilities.shared import client, tree
