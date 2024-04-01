f3c90fedfbb16769998b4f711078648dbe4187a6
Tobias Soltvedt <tobsol01@gmail.com>
2024-04-01 20:25:52 +0200

    updating gif nolonger uses an outdated CTX

--- commands/emulator_commands.py
+++ commands/emulator_commands.py
@@ -19,82 +19,71 @@ async def pokemon(ctx):
     for _ in range(1500//emu.skipped_frames): #time needed to get to titlescreen
         emu.tick()
     file = discord.File(fp=emu.make_gif(), filename="emulator.gif")
-    msg = await ctx.edit_original_response(attachments=[file], view=EmulatorController(ctx))
+    msg = await ctx.edit_original_response(attachments=[file], view=EmulatorController())
 
     await msg.create_thread(name="Discuss Tottoko Hamutarou!")
 
 
 class EmulatorController(discord.ui.View):
 
-    def __init__(self, message_ctx):
+    def __init__(self):
         super().__init__(timeout=None)
-        self.message_ctx = message_ctx
 
-    async def update_gif(self):
+    async def update_gif(self, interaction: discord.Interaction):
         file = discord.File(fp=emu.make_gif(), filename="emulator.gif")
 
-        await self.message_ctx.edit_original_response(attachments=[file], view=self)
+        await interaction.response.edit_message(attachments=[file], view=self)
 
     @discord.ui.button(emoji="🅰️", style=discord.ButtonStyle.red, row=0)
     async def A_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
         emu.sim_button_time("A", 200)
 
-        await self.update_gif()
+        await self.update_gif(interaction)
 
     @discord.ui.button(emoji="🔼", style=discord.ButtonStyle.blurple, row=0)
     async def up_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
         emu.sim_button_time("up", 200)
 
-        await self.update_gif()
+        await self.update_gif(interaction)
 
     @discord.ui.button(emoji="🅱️", style=discord.ButtonStyle.red, row=0)
     async def B_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
         emu.sim_button_time("B", 200)
 
-        await self.update_gif()
+        await self.update_gif(interaction)
 
     @discord.ui.button(emoji="◀️", style=discord.ButtonStyle.blurple, row=1)
     async def left_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
         emu.sim_button_time("Left", 200)
 
-        await self.update_gif()
+        await self.update_gif(interaction)
 
     @discord.ui.button(emoji="🔽", style=discord.ButtonStyle.blurple, row=1)
     async def down_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
         emu.sim_button_time("Down", 200)
 
-        await self.update_gif()
+        await self.update_gif(interaction)
 
     @discord.ui.button(emoji="▶️", style=discord.ButtonStyle.blurple, row=1)
     async def right_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
         emu.sim_button_time("Right", 200)
 
-        await self.update_gif()
+        await self.update_gif(interaction)
 
     @discord.ui.button(emoji="📃", style=discord.ButtonStyle.grey, row=2)
     async def start_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
         emu.sim_button_time("start", 200)
-
-        await self.update_gif()
+        await self.update_gif(interaction)
 
     @discord.ui.button(emoji="⌚", style=discord.ButtonStyle.grey, row=2)
     async def empty_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
         for _ in range(500):
             emu.tick()
 
-        await self.update_gif()
+        await self.update_gif(interaction)
 
     @discord.ui.button(emoji="🔎", style=discord.ButtonStyle.grey, row=2)
     async def select_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
         emu.sim_button_time("select", 200)
 
-        await self.update_gif()
+        await self.update_gif(interaction)
