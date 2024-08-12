0651c0bf20e08964b881cc6660804b6bbfd9564c
tobbensol <tobsol01@gmail.com>
2024-08-12 16:31:09 +0200

    fixed emulator breaking on bot reset

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from commands.wordle import initialize_wordle, wordle, guess_wordle
 from commands.random_commands import this_dude, weave, pet_elga3, thanos_snapped, trout
 from commands.help import help_command
 from commands.guess_that_pin import guess_that_pin, initialize_guess_that_pin
-from commands.emulator_commands import pokemon, emu
+from commands.emulator_commands import pokemon, emu, EmulatorController
 from the_lab.petting import petting
 
 from utilities.settings import guild_id, testing, client, tree
@@ -22,8 +22,13 @@ if testing:
 
 @client.event
 async def on_ready():
+    # sync commands
     await tree.sync(guild=discord.Object(id=guild_id))
 
+    # make emulator buttons work
+    client.add_view(EmulatorController())
+
+    # initiate scheduled items
     await initialize_wordle()
     await initialize_guess_that_pin()
     client.loop.create_task(schedule_post_free_games())
--- commands/emulator_commands.py
+++ commands/emulator_commands.py
@@ -39,38 +39,38 @@ class EmulatorController(discord.ui.View):
 
         await self.update_gif(interaction)
 
-    @discord.ui.button(emoji="🅰️", style=discord.ButtonStyle.red, row=0)
+    @discord.ui.button(emoji="🅰️", style=discord.ButtonStyle.red, row=0, custom_id="A_Button")
     async def A_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
         await self.update(interaction, "A", 200)
 
-    @discord.ui.button(emoji="🔼", style=discord.ButtonStyle.blurple, row=0)
+    @discord.ui.button(emoji="🔼", style=discord.ButtonStyle.blurple, row=0, custom_id="Up_Button")
     async def up_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
         await self.update(interaction, "up", 200)
 
-    @discord.ui.button(emoji="🅱️", style=discord.ButtonStyle.red, row=0)
+    @discord.ui.button(emoji="🅱️", style=discord.ButtonStyle.red, row=0, custom_id="B_Button")
     async def B_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
         await self.update(interaction, "B", 200)
 
-    @discord.ui.button(emoji="◀️", style=discord.ButtonStyle.blurple, row=1)
+    @discord.ui.button(emoji="◀️", style=discord.ButtonStyle.blurple, row=1, custom_id="Left_Button")
     async def left_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
         await self.update(interaction, "left", 200)
 
-    @discord.ui.button(emoji="🔽", style=discord.ButtonStyle.blurple, row=1)
+    @discord.ui.button(emoji="🔽", style=discord.ButtonStyle.blurple, row=1, custom_id="Down_Button")
     async def down_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
         await self.update(interaction, "down", 200)
 
-    @discord.ui.button(emoji="▶️", style=discord.ButtonStyle.blurple, row=1)
+    @discord.ui.button(emoji="▶️", style=discord.ButtonStyle.blurple, row=1, custom_id="Right_Button")
     async def right_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
         await self.update(interaction, "right", 200)
 
-    @discord.ui.button(emoji="📃", style=discord.ButtonStyle.grey, row=2)
+    @discord.ui.button(emoji="📃", style=discord.ButtonStyle.grey, row=2, custom_id="Select_Button")
     async def start_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
         await self.update(interaction, "start", 200)
 
-    @discord.ui.button(emoji="⌚", style=discord.ButtonStyle.grey, row=2)
+    @discord.ui.button(emoji="⌚", style=discord.ButtonStyle.grey, row=2, custom_id="Wait_Button")
     async def empty_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
         await self.update(interaction, None, 200)
 
-    @discord.ui.button(emoji="🔎", style=discord.ButtonStyle.grey, row=2)
+    @discord.ui.button(emoji="🔎", style=discord.ButtonStyle.grey, row=2, custom_id="Settings_Button")
     async def select_button(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
         await self.update(interaction, "select", 200)
