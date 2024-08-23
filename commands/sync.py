57dfbdfa04830602142ccd18af10a78a53df614d
Krissibro <136352042+Krissibro@users.noreply.github.com>
2024-08-23 19:20:30 +0200

    Fixed the command syncing problem by moving the syncing into a command

---
+++ commands/sync.py
@@ -0,0 +1,42 @@
+
+
+import discord
+from discord.ext import commands
+from utilities.settings import bot
+from typing import Literal, Optional
+
+
+# https://about.abstractumbra.dev/discord.py/2023/01/29/sync-command-example.html
+@bot.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(ctx: commands.Context, guilds: commands.Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+            print("synced")
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
