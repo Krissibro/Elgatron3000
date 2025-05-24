1741e22edc045e18178f7dfe0ebd6466e4e940aa
tobbensol <tobsol01@gmail.com>
2025-05-24 14:42:33 +0200

    removed "command name" argument from execute_command

--- command_objects/MessagingInfo.py
+++ command_objects/MessagingInfo.py
@@ -23,14 +23,14 @@ class MessagingInfo(CommandInfo):
             title=f"Command: {self.command_name}",
             description=f"Message: {self.message}"
         )
-        embed.add_field(name="User:", value=f"{self.get_mention()}", inline=False)
+        if self.user is not None:
+            embed.add_field(name="User:", value=f"{self.get_mention()}", inline=False)
         embed.add_field(name="Amount:", value=f"{self.remaining}/{self.amount}", inline=True)
         embed.add_field(name="Interval:", value=f"{timedelta(seconds=self.interval)}", inline=True)
         return embed
 
     def make_overview(self, index, embed):
-        formatted_command_string = " ".join(self.command_name.split("_")).capitalize()
-        embed.add_field(name=f"{char_to_emoji(index)} {formatted_command_string}",
+        embed.add_field(name=f"{char_to_emoji(index)} {self.command_name}",
                         value=f"{self.get_mention()}\n{self.message}",
                         inline=False)
 
