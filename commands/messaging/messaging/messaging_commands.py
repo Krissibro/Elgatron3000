6f2f1914f5cb9a813b6b75be3417abf2781deb17
Tobias Soltvedt <tobsol01@gmail.com>
2025-10-24 18:14:18 +0200

    split messaging commands into two

---
+++ commands/messaging/messaging/messaging_commands.py
@@ -0,0 +1,47 @@
+from typing import Union
+
+import discord
+
+from discord import app_commands
+from discord.ext import commands
+
+from commands.messaging.messaging.messaging_model import execute_command, get_attention_internal, annoy_internal, dm_spam_internal
+
+from utilities.helper_functions import parse_time
+from utilities.elgatron import Elgatron
+
+
+# TODO figure out how converters work 
+# https://discordpy.readthedocs.io/en/latest/interactions/api.html?highlight=app_commands#transformerss
+class MessagingCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(
+        name="dm_spam",
+        description="Annoy someone as many times as you would like with a given interval!",
+    )
+    async def dm_spam(self, ctx, user: discord.User, message: str, amount: int, interval: str):
+        parsed_interval:int = parse_time(interval)
+        await execute_command(ctx, dm_spam_internal, user, message, amount, parsed_interval, ctx.channel)
+
+    @app_commands.command(
+        name="get_attention",
+        description="Ping someone once every 10 seconds 100 times or until they react"
+    )
+    async def get_attention(self, ctx, target: Union[discord.User, discord.Role, None], message: str = "WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP",
+                            amount: int = 100, interval: str = "10s"):
+        parsed_interval: int = parse_time(interval)
+        await execute_command(ctx, get_attention_internal, target, message, amount, parsed_interval, ctx.channel)
+
+    @app_commands.command(
+        name="annoy",
+        description="Spam a message at someone!",
+    )
+    async def annoy(self, ctx, message: str, amount: int, interval: str, target: Union[discord.User, discord.Role, None] = None):
+        parsed_interval: int = parse_time(interval)
+        await execute_command(ctx, annoy_internal, target, message, amount, parsed_interval, ctx.channel)
+
+
+async def setup(bot: Elgatron):
+    await bot.add_cog(MessagingCommands(bot), guild=discord.Object(id=bot.guild_id))
--- commands/messaging/messaging/messaging_model.py
+++ commands/messaging/messaging/messaging_model.py
@@ -1,17 +1,10 @@
-from typing import Optional, Callable, Union
-
 import discord
 import asyncio
 
-from discord import app_commands
-from discord.ext import commands
-
 from commands.messaging.MessagingInfo import MessagingInfo
+from typing import Callable, Union 
 
-from utilities.helper_functions import parse_time
-from utilities.elgatron import Elgatron
-from utilities.settings import active_commands 
-
+from utilities.settings import active_commands
 
 async def execute_command(ctx, internal_function: Callable, target: Union[discord.User, discord.Role, None], message: str, amount: int, interval: int, channel: discord.TextChannel) -> None:
     """
@@ -105,38 +98,3 @@ async def annoy_internal(ctx, messaging_info: MessagingInfo) -> None:
         await asyncio.sleep(messaging_info.interval)
 
     await messaging_info.delete_messages()
-
-# TODO figure out how converters work 
-# https://discordpy.readthedocs.io/en/latest/interactions/api.html?highlight=app_commands#transformerss
-class MessagingCommands(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @app_commands.command(
-        name="dm_spam",
-        description="Annoy someone as many times as you would like with a given interval!",
-    )
-    async def dm_spam(self, ctx, user: discord.User, message: str, amount: int, interval: str):
-        parsed_interval:int = parse_time(interval)
-        await execute_command(ctx, dm_spam_internal, user, message, amount, parsed_interval, ctx.channel)
-
-    @app_commands.command(
-        name="get_attention",
-        description="Ping someone once every 10 seconds 100 times or until they react"
-    )
-    async def get_attention(self, ctx, target: Union[discord.User, discord.Role, None], message: str = "WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP",
-                            amount: int = 100, interval: str = "10s"):
-        parsed_interval: int = parse_time(interval)
-        await execute_command(ctx, get_attention_internal, target, message, amount, parsed_interval, ctx.channel)
-
-    @app_commands.command(
-        name="annoy",
-        description="Spam a message at someone!",
-    )
-    async def annoy(self, ctx, message: str, amount: int, interval: str, target: Union[discord.User, discord.Role, None] = None):
-        parsed_interval: int = parse_time(interval)
-        await execute_command(ctx, annoy_internal, target, message, amount, parsed_interval, ctx.channel)
-
-
-async def setup(bot: Elgatron):
-    await bot.add_cog(MessagingCommands(bot), guild=discord.Object(id=bot.guild_id))
