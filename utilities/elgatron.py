093325bb5e992ca792fe7cf12822ac8f319a710a
tobbensol <tobsol01@gmail.com>
2025-10-31 16:23:02 +0100

    bot now loads commands on both windows and linux

--- utilities/elgatron.py
+++ utilities/elgatron.py
@@ -25,15 +25,17 @@ class Elgatron(Bot):
 
     async def setup_hook(self) -> None:
         for path in glob("./commands/**/*_commands.py", recursive=True):
-            # files are in the format: "./x/y.py" 
+            # files are in the format: "./x/y.py"
             # this turns it to: "x.y"
-            formatted_path = path[2:-3].replace("/", ".")
+            formatted_path = path[2:-3].replace("/", ".") # linux
+            formatted_path = formatted_path.replace("\\", ".") # windows
             await self.load_extension(formatted_path)
-        
+
 
         if self.testing:
             for path in glob("./the_lab/**/*_commands.py", recursive=True):
                 formatted_path = path[2:-3].replace("/", ".")
+                formatted_path = formatted_path.replace("\\", ".")
                 await self.load_extension(formatted_path)
 
         self.scheduler.start()
