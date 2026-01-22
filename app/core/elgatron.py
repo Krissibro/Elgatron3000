e631009aa2066b06b1784f768e2b1c8ce1fbe3d5
tobbensol <tobsol01@gmail.com>
2026-01-22 12:39:13 +0100

    added DB (there are no models tho)

--- app/core/elgatron.py
+++ app/core/elgatron.py
@@ -4,6 +4,7 @@ import discord
 import logging
 import json
 
+from tortoise import Tortoise
 from pathlib import Path
 from discord.ext.commands import Bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -13,7 +14,7 @@ from app.core.elgaTree import ElgaTree
 
 class Elgatron(Bot):
     def __init__(self):
-        with open("./static/config.json", "r") as f:
+        with open("static/config.json", "r") as f:
             contents = json.load(f)
 
         self.guild_id = contents["guild"]
@@ -22,7 +23,7 @@ class Elgatron(Bot):
         self.wordle_channel_id = contents["wordle_channel_id"]
         self.testing_channel_id = contents["testing_channel_id"]
 
-        self.scheduler: AsyncIOScheduler = AsyncIOScheduler(timezone='Europe/Oslo')
+        self.scheduler: AsyncIOScheduler = AsyncIOScheduler(timezone="Europe/Oslo")
         self.active_commands: ActiveCommands = ActiveCommands()
 
         self.logger = logging.getLogger("discord")
@@ -30,16 +31,33 @@ class Elgatron(Bot):
         super().__init__(intents=self.get_intents(), command_prefix="/", tree_cls=ElgaTree)
 
     async def setup_hook(self) -> None:
+        path = Path("./app/commands").resolve()
+        await Tortoise.init(
+            db_url=f"sqlite://{path}",
+            modules={"models": ["app.models"]}
+        )
+        await Tortoise.generate_schemas()
+
+        await self.load_extension("./app/commands")
+
         self.scheduler.start()
         self.scheduler.print_jobs()
 
-        # TODO init DB
+    async def on_ready(self):
+        if self.testing:
+            # discord.Object(id=...) is better than bot.get_guild(...) because it works when disconnected
+            await self.tree.sync(guild=discord.Object(id=self.guild_id))
 
-        await self.load_extension("./app/commands")
+        print("Ready!")
+
+    async def close(self) -> None:
+        await Tortoise.close_connections()
+        await super().close()
 
     async def load_extension(self, name: str, *, package: Optional[str] = None) -> None:
         path = Path(name)
         for file_path in path.glob("**/*_commands.py"):
+            print(file_path)
             file_parts = file_path.parts
 
             if "the_lab" in file_parts and (not self.testing):
@@ -50,13 +68,6 @@ class Elgatron(Bot):
                 await super().load_extension(name=formatted_path, package=package)
             except Exception as e:
                 self.logger.error(f"Failed to load extension {name}.", exc_info=e)
-
-    async def on_ready(self):
-        if self.testing:
-            # discord.Object(id=...) is better than bot.get_guild(...) because it works when disconnected
-            await self.tree.sync(guild=discord.Object(id=self.guild_id))
-
-        print("Ready!")
 
     @staticmethod
     def get_intents():
