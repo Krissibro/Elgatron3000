0854cd4db5f6a12332daefd0049163ed9e569e81
tobbensol <tobsol01@gmail.com>
2026-01-24 19:52:21 +0100

    wordle_model_db.py is now "stateless" and only a db manager

---
+++ app/commands/the_lab/wordle_db.py
@@ -0,0 +1,87 @@
+import numpy as np
+import random
+import discord
+
+from datetime import datetime
+from typing import Union
+
+from app.models.wordle_model import WordleGuess, WordleGame
+from app.utilities.errors import ElgatronError
+
+
+class WordleDB:
+    def __init__(self, testing: bool=False):
+        self.testing: bool = testing
+
+        self.valid_words = set(np.genfromtxt('./static/word_lists/valid-words.csv', delimiter=',', dtype=str).flatten()) # all words
+        self.word_bank = set(np.genfromtxt('./static/word_lists/word-bank.csv', delimiter=',', dtype=str).flatten()) # words that can be chosen
+        whitelisted_words = set(np.genfromtxt('./static/word_lists/whitelisted-words.csv', delimiter=',', dtype=str).flatten()) # custom words
+
+        self.valid_words |= whitelisted_words
+        self.word_bank |= whitelisted_words
+
+    async def new_game(self) -> WordleGame:
+        random_word = random.choice(tuple(self.word_bank)).upper()
+        return await WordleGame.create(
+            word=random_word,
+            date=datetime.today()
+        )
+
+    async def guess_word(self, guessed_word: str, user: Union[discord.User, discord.Member]) -> None:
+        game = await self.get_current_game()
+        await game.fetch_related("guesses")
+
+        guess = WordleGuess(
+            guesser_id =user.id,
+            guesser_name=user.display_name,
+            word=guessed_word.strip().upper(),
+            time=datetime.now(),
+            game=game
+        )
+
+        self.validate_wordle_guess(guess, game)
+        await guess.save()
+
+        if game.first_guess_time is None:
+            game.first_guess_time = guess.time
+
+        if guess.word == game.word:
+            game.finished = True
+            game.final_guess_time = guess.time
+
+        await game.save()
+
+    def validate_wordle_guess(self, guess: WordleGuess, game: WordleGame) -> None:
+        """
+        raises error if guessed word is invalid
+        :param guess: the guessed word.
+        :param game: the game object.
+        """
+        existing_ids = {g.guesser_id for g in game.guesses}
+        existing_words = {g.word for g in game.guesses}
+
+        if guess.word == game.word:
+            raise ElgatronError("The daily wordle has already been guessed")
+
+        if not self.testing:
+            if guess.guesser_id in existing_ids:
+                raise ElgatronError(f"{guess.guesser_name} has already guessed")
+            if len(guess.word) != 5:
+                raise ElgatronError("The word must be 5 letters long")
+            if guess.word not in self.valid_words:
+                raise ElgatronError(f'"{guess.word}" is not a valid word')
+        if guess.word in existing_words:
+            raise ElgatronError(f'"{guess.word}" has already been guessed')
+
+    async def get_current_game(self) -> WordleGame:
+        game = (
+            await WordleGame
+            .filter(date__date=datetime.today())
+            .prefetch_related("guesses")
+            .first()
+        )
+
+        if game is None:
+            game = await self.new_game()
+
+        return game
--- app/models/wordle_model.py
+++ app/models/wordle_model.py
@@ -4,10 +4,11 @@ from tortoise import fields
 
 class WordleGame(Model):
     id:                 fields.Field[int]       = fields.IntField(primary_key=True)
-    date:               fields.Field[datetime] = fields.DatetimeField(null=True)
+    date:               fields.Field[datetime]  = fields.DatetimeField(null=True)
     word:               fields.Field[str]       = fields.CharField(max_length=16)
     first_guess_time:   fields.Field[datetime]  = fields.DatetimeField(null=True)
     final_guess_time:   fields.Field[datetime]  = fields.DatetimeField(null=True)
+    finished:           fields.Field[bool]      = fields.BooleanField(default=False)
     guesses:            fields.ReverseRelation["WordleGuess"]
 
     def __str__(self):
@@ -15,13 +16,13 @@ class WordleGame(Model):
 
 
 class WordleGuess(Model):
-    id:         fields.Field[int]                       = fields.IntField(primary_key=True)
-    word:      fields.Field[str]                       = fields.CharField(max_length=16)
-    guesser_id:    fields.Field[int]                       = fields.IntField()
-    guesser_name:  fields.Field[str]                       = fields.CharField(max_length=64)
-    time: fields.Field[datetime]                  = fields.DatetimeField(null=True)
+    id:             fields.Field[int]                       = fields.IntField(primary_key=True)
+    word:           fields.Field[str]                       = fields.CharField(max_length=16)
+    guesser_id:     fields.Field[int]                       = fields.IntField()
+    guesser_name:   fields.Field[str]                       = fields.CharField(max_length=64)
+    time:           fields.Field[datetime]                  = fields.DatetimeField(null=True)
 
-    game:       fields.ForeignKeyRelation["WordleGame"]   = fields.ForeignKeyField(
+    game:       fields.ForeignKeyRelation["WordleGame"]     = fields.ForeignKeyField(
         model_name="models.WordleGame",
         related_name="guesses",
         on_delete=fields.CASCADE
