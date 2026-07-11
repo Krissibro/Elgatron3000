b545fee5cd7b7869ae8794ae6dbc2c7eae1ec46e
tobbensol <tobsol01@gmail.com>
2026-07-11 10:19:41 +0200

    renamed bot.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from typing import Optional
+
+from dotenv import load_dotenv
+
+from utilities.elgatron import Elgatron
+
+
+def main():
+    load_dotenv(".env")
+
+    token: Optional[str] = os.getenv("TOKEN")
+    if not token:
+        raise ValueError("TOKEN not found in environment")
+
+    bot = Elgatron()
+    bot.run(token)
+
+if __name__ == "__main__":
+    main()
